06d3d41ae2eaa87103ec940086eb6d583e8b7289
josemalcher <contato@josemalcher.net>
2020-01-26 22:31:07 -0300

    7 - Segundo plugin - painel personalizado Parte 2/4

---
+++ wordpress/wp-content/plugins/curso02_painel_personalizado/curso02_painel_personalizado.php
@@ -0,0 +1,71 @@
+<?php
+/*
+Plugin Name: Curso 02 - Painel Personalizado
+Plugin URI: http://#
+Description: Exemplo 02 do curso - Painel personalizado
+Version: 1.0.0
+Author: José Malcher Junior
+Author URI: https://josemalcher.net
+License: GPLv2 or later
+Text Domain: exemplo
+*/
+/*
+remove_action('welcome_panel', 'wp_welcome_panel');
+function my_welcome_panel(){
+    */ ?><!--
+    <div class="welcome-panel-content">
+        <h3>Bem vindo ao Painel administrativo</h3>
+        <p>Siga nossas redes sociais</p>
+        <div id="icons">
+            <a href="#" target="_blank">
+                <img src="<? /*=plugins_url()*/ ?>/curso02_painel_personalizado/img/facebook-circle-color.png" alt="Facebook">
+            </a>
+            <a href="#">
+                <img src="<? /*=plugins_url()*/ ?>/curso02_painel_personalizado/img/youtube-circle-color.png" alt="Youtube">
+            </a>
+        </div>
+    </div>
+--><?php
+/*}
+add_action('welcome_panel', 'my_welcome_panel');
+*/
+
+// Refatoranto
+class Curso02Painel
+{
+    private static $instance;
+
+    public static function getInstance()
+    {
+        if(self::$instance == NULL){
+            self::$instance = new self();
+        }
+    }
+    private function __construct()
+    {
+        // desativa a action welcome_panel
+        remove_action('welcome_panel', 'wp_welcome_panel');
+        add_action('welcome_panel', array($this, 'my_welcome_panel'));
+    }
+
+    public function my_welcome_panel()
+    {
+        ?>
+        <div class="welcome-panel-content">
+            <h3>Bem vindo ao Painel administrativo</h3>
+            <p>Siga nossas redes sociais</p>
+            <div id="icons">
+                <a href="#" target="_blank">
+                    <img src="<?=plugins_url() ?>/curso02_painel_personalizado/img/facebook-circle-color.png"
+                         alt="Facebook">
+                </a>
+                <a href="#">
+                    <img src="<?=plugins_url() ?>/curso02_painel_personalizado/img/youtube-circle-color.png"
+                         alt="Youtube">
+                </a>
+            </div>
+        </div>
+        <?php
+    }
+}
+Curso02Painel::getInstance();
